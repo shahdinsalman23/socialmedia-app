441acade6c5609561b36925774e6592d416f8f1e
Shahdin Salman <125382694+shahdinsalman23@users.noreply.github.com>
2025-08-17 13:03:16 +0500

    updated

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,7 +4,6 @@ const Post = () => {
     return (
         <>
             <div class="card" style={{ width: "18rem" }}>
-                <img src="..." class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
